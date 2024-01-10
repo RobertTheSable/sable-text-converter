c727d8f9faf31cc176c75c48ac21b0c9412955c0
RobertTheSable <robertthesable@gmail.com>
2024-01-10 17:46:00 -0600

    Minor cleanup.
    - Replace usages of list with file glob.
    - Remove NFD decomposition step, as NFC implies it.

--- src/font/normalize.cpp
+++ src/font/normalize.cpp
@@ -12,20 +12,12 @@ namespace sable {
 std::string normalize(const std::string &in)
 {
     UErrorCode err = U_ZERO_ERROR;
-    std::string sinkDestNFD, sinkDestNFC;
-    icu::StringByteSink sinkNFD(&sinkDestNFD);
+    std::string sinkDestNFC;
     icu::StringByteSink sinkNFC(&sinkDestNFC);
-
-    if (auto instance = icu::Normalizer2::getNFDInstance(err); U_FAILURE(err)) {
-        throw std::runtime_error("couldn't get the NFD instance.");
-    } else if (instance->normalizeUTF8(0, in, sinkNFD, nullptr, err); U_FAILURE(err)) {
-        throw std::runtime_error("cound't perform NFD normalization.");
-    }
 
     if (auto instance = icu::Normalizer2::getNFCInstance(err); U_FAILURE(err)) {
         throw std::runtime_error("couldn't get the NFC instance.");
-    } else if (instance->normalizeUTF8(0, sinkDestNFD, sinkNFC, nullptr, err); U_FAILURE(err)) {
-
+    } else if (instance->normalizeUTF8(0, in, sinkNFC, nullptr, err); U_FAILURE(err)) {
         throw std::runtime_error("cound't perform NFC normalization.");
     }
 
